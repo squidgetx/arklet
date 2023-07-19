1b032ca5c53e5226c5232fc3863c21e6abe61f65
Sylvan Zheng <squidgetx@gmail.com>
2023-07-19 17:49:13 -0400

    get rid of unnecessary admin

--- ark/admin.py
+++ ark/admin.py
@@ -1,7 +1,6 @@
 """Django Admin models for Arklet."""
 
 from django.contrib import admin, messages
-from django.shortcuts import render
 from ark.models import Ark, Key, Naan, Shoulder, User
 
 
@@ -15,25 +14,6 @@ class NaanAdmin(admin.ModelAdmin):
     """Django Admin model for Name Assignment Authority Number bearing organizations."""
 
     list_display = ["name", "naan"]
-
-
-@admin.register(Shoulder)
-class ShoulderAdmin(admin.ModelAdmin):
-    """Django Admin model for ARK shoulders."""
-
-    list_display = ["shoulder", "name", "naan"]
-
-
-@admin.register(Ark)
-class ArkAdmin(admin.ModelAdmin):
-    """Django Admin model for ARKs.
-
-    In practice, stock Django Admin doesn't work well for large randomly sorted tables.
-    This view will tend to take a long time to load as a full table count query is run.
-    """
-
-    list_display = ["ark", "url"]
-    show_full_result_count = False
 
 
 @admin.register(Key)
