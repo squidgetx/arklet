f37dd069d64ce35e0173122a3d0e6f9a3bb5d1c3
Sylvan Zheng <squidgetx@gmail.com>
2023-08-04 19:11:52 -0700

    revert

--- nginx/certbot_renew.sh
+++ nginx/certbot_renew.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 DOMAIN_FLAGS="-d ark.frick.org"
-NGINX_RELOAD_CMD="nginx -s reload"
 EMAIL=saz310@nyu.edu
 
 certbot certonly --non-interactive --nginx --agree-tos --email $EMAIL $DOMAIN_FLAGS
 
-# Check if certbot command was successful (exit code 0) and reload Nginx
-if [ $? -eq 0 ]; then
-    $NGINX_RELOAD_CMD
-fi
