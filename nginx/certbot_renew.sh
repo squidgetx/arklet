a24282925e46762f4891e462612d9439ab963ada
Sylvan Zheng <squidgetx@gmail.com>
2023-08-09 13:02:00 -0700

    ssl attempt

--- nginx/certbot_renew.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DOMAIN_FLAGS="-d ark.frick.org"
-EMAIL=saz310@nyu.edu
-
-certbot certonly --non-interactive --nginx --agree-tos --email $EMAIL $DOMAIN_FLAGS
-
---
+++ nginx/ssl_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script in conjunction with nginx.ssl.conf enables SSL 
+# by requesting a certificate from Lets Encrypt and then replacing
+# nginx.conf with nginx.ssl.conf, which is assumed to exist 
+
+DOMAIN_FLAGS="-d ark.frick.org"
+EMAIL=saz310@nyu.edu
+
+certbot certonly --non-interactive --nginx --agree-tos --email $EMAIL $DOMAIN_FLAGS;
+cp /nginx.ssl.conf /etc/nginx/conf.d/nginx.conf;
+/etc/init.d/nginx reload;
+
